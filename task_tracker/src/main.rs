7d8870206e74c6b90533df19a6133fe6a6c2ec58
Marciano Mavungo <marcmav@proton.me>
2026-09-03 23:31:59 +0100

    add command module to structure each command

--- task_tracker/src/main.rs
+++ task_tracker/src/main.rs
@@ -1,12 +1,4 @@
-use std::env;
+mod commands;
 
 fn main() {
-    let args: Vec<String> = env();
-    if args.len() < 2 {
-        println!("wrong input");
-        println!("./{} <args>", args[0]);
-    }
-    for arg in args.iter() {
-        println!("{arg}");
-    }
 }
---
+++ task_tracker/target/debug/task_tracker.d
@@ -0,0 +1,1 @@
+/home/marcio/Code/src/closed-source/sandbox/task_tracker/target/debug/task_tracker: /home/marcio/Code/src/closed-source/sandbox/task_tracker/src/main.rs
